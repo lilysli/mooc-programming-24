ee546a5a752f2f990e0cc4417435860786a4dd45
Lily Sijia Li <lilysijiali@gmail.com>
2025-08-25 21:40:48 +0200

    part 4 - 01 exercise passed test

--- part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
+++ part04-01_hello_visual_studio_code/src/hello_visual_studio_code.py
@@ -1,1 +1,10 @@
-# Write your solution here
+while True:
+    editor = input("Editor:").lower()
+
+    if editor == "word" or editor == "notepad":
+        print("awful")
+    elif editor == "visual studio code":
+        print("an excellent choice!")
+        break
+    else:
+        print("not good")
